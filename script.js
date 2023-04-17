183f6ca3b164c2fb7ab91f68aba8744b577aea69
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2023-04-17 23:56:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,640 @@
+// console.log("hello, i am hanish");
+// alert("hanish");
+// document.write("Hello Everyone How are you");
+
+// console.log("hello World", 4 + 4);
+// console.warn("this is warn");
+// console.error("thsi is error");
+// console.assert(6 == 9);
+
+// let num1 = 10;
+// let num2 = 20;
+// let c = num1 + num2;
+// console.log(c);
+
+// let story = "hello everyone my name is hanish tharwani";
+// console.log(story + " i am 10 years old");
+
+// let num = 100;
+// console.log(num);
+// let name = "hanish";
+// console.log(name);
+// let hello = true;
+// console.log(hello);
+// let compare = 2 > 4;
+// console.log(compare);
+// let firstName = undefined;
+// console.log(firstName);
+// let lastName = null;
+// console.log(lastName);
+// let name1;
+// console.log(name1);
+
+// let exam_percent = {
+//   hanish: 40,
+//   naman: 70,
+//   harry: 90,
+// };
+
+// exam_percent.hanish = 95;
+// exam_percent["naman"] = 60;
+
+// console.log(exam_percent.hanish);
+// console.log(exam_percent.naman);
+// console.log(exam_percent.harry);
+
+// colors = ["red", "blue", "green", 1, 2];
+// colors[2] = "purple";
+// console.log(colors);
+
+// let num1 = 110;
+// let num2 = 200;
+// console.log("the sum of", num1, "and", num2, "is :", num1 + num2);
+// console.log("the subtraction of", num1, "and", num2, "is :", num1 - num2);
+// console.log("the multiplication of", num1, "and", num2, "is :", num1 * num2);
+// console.log("the division of", num1, "and", num2, "is :", num1 / num2);
+
+// let name = window.prompt("what is your name");
+// console.lof(name);
+
+// let a = 10;
+// a++;
+// a+=2
+// console.log(a);
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && false);
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
+
+// console.log(!false);
+// console.log(!true);
+
+// function greet(name, lastName) {
+//   console.log("hello", name, lastName, "how are you");
+//   console.log("hello everyone, this is my friend " + name + " " + lastName);
+// }
+// greet("hanish", "tharwani");
+
+// function square(num) {
+//   let c = num * num;
+//   return c;
+// }
+
+// let result = square(2);
+// console.log(result);
+
+// function Cube(num) {
+//   let cube = num * num * num;
+//   return cube;
+// }
+
+// let resul = Cube(3);
+// console.log(resul);
+
+// let num1 = 19;
+// if (num1 > 3) {
+//   console.log("num1 is greater than 10");
+// } else if (num1 > 9) {
+//   console.log("num1 is greater than 20");
+// } else if (num1 > 8) {
+//   console.log("num1 is greater than 1");
+// } else {
+//   console.log("enjoy your life😎");
+// }
+
+// let age = 18;
+// if (age < 18) {
+//   console.log("You cannot vote because you are below 18");
+// } else if (age < 18) {
+//   console.log("You can vote because your age above 18");
+// } else {
+//   console.log("You can vote it depends upon you");
+// }
+
+// function factorial(num) {
+//   let fact = num * (num - 1);
+//   return fact;
+// }
+
+// let result = factorial(4);
+// console.log(result);
+
+// let age = 10;
+// if (age > 9) {
+//   console.log("sab badiya he");
+// }
+// if (age > 8) {
+//   console.log("sab or badiya he");
+// } else {
+//   console.log("sab ganda he");
+// }
+
+// for (let i = 0; i <= 11; i++) {
+//   console.log(i);
+// }
+
+// let num = 2;
+// for (let i = 1; i < 11; i++) {
+//   console.log(num, "X", i, "=", num * i);
+// }
+
+// let num = 10;
+// for (let i = 10; i > 0; i -= 1) {
+//   console.log(num + " X " + i + " = " + num * i);
+// }
+
+// let array = [1, 2, 3, 4, 5, 6, 7];
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// let num = 2;
+// let i = 1;
+// while (i < 11) {
+//   console.log(num, "X", i, "=", num * i);
+//   i++;
+// }
+
+// var num = 2;
+// var i = 1;
+// do {
+//   console.log(num, "X", i, "=", num * i);
+//   i++;
+// } while (i < 11);
+
+// let num = 2;
+// for (let i = 1; i < 11; i++) {
+//   if (i == 6) {
+//     continue;
+//   }
+//   console.log(num, "X", i, "=", num * i);
+// }
+
+// let fruits = ["apple", "banana", "santra", "kadu", "pyaaz"];
+// console.log(fruits.length);
+// console.log(fruits.pop());
+// console.log(fruits.push());
+// console.log(fruits.shift());
+// console.log(fruits.toString());
+// console.log(fruits.sort());
+
+// let string1 = "hello my name is hanish tharwani";
+// console.log(string1.slice(0, 3));
+
+// let date = new Date();
+// console.log(date.getMinutes());
+
+// let num = 10;
+// if (num > 10 && num == 10) {
+//   console.log("num is greater than 10 and num is less than 10");
+// } else if (num > 10 || num < 10) {
+//   console.log("num is greater than 10 or num ois equal to 10");
+// } else {
+//   console.log("happy");
+// }
+
+// let num1 = 10;
+// let num2 = 20;
+// let c = num1;
+// num1 = num2;
+// num2 = c;
+// console.log(num1);
+// console.log(num2);
+
+// for (let i = 1; i < 11; i++) {
+//   tableOf = 2;
+//   console.log(` ${tableOf} X ${i} = ${tableOf * i}`);
+// }
+
+// let i = 1;
+// while (i < 11) {
+//   table = 5;
+//   console.log(`${table} X ${i} = ${table * i}`);
+//   i++;
+// }
+
+// let i = 1;
+// do {
+//   num = 10;
+//   console.log(`${num} X ${i} = ${num * i}`);
+//   i++;
+// } while (i < 11);
+
+// for (let i = 1; i <= 10; i++) {
+//   num = 2;
+//   console.log(`${num} X ${i} = ${num * i}`);
+// }
+
+// const sum = (num) => {
+//   let Sum = num + num;
+//   return Sum;
+// };
+
+// let result = sum(10);
+// console.log(result);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let elements in arr) {
+//   console.log(elements);
+// }
+
+// for (let elements of arr) {
+//   console.log(elements);
+// }
+
+// let arr = ["apple", "mango", "orange", "pinepal", "cherry"];
+// arr.forEach(function (element, index, array) {
+//   console.log(element, index, array);
+// });
+
+// let date = new Date();
+// let day = date.getDay();
+// let arr = [
+//   "MONDAY",
+//   "TUESDAY",
+//   "WEDNESDAY",
+//   "THURSDAY",
+//   "FRIDAY",
+//   "SATURDAY",
+//   "SUNDAY",
+// ];
+// console.log("Today is :", arr[day]);
+
+// function print_current_page() {
+//   window.print();
+// }
+
+// let date = new Date();
+// let mm = date.getMonth() + 1;
+// let dd = date.getDate();
+// let yy = date.getFullYear();
+
+// console.log(mm, "/", dd, "/", yy);
+
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// let s = (a + b + c) / 2;
+// let Area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+// console.log("The area of scalane triangle is :", Area);
+
+// let year = 2016;
+// if (year % 4 == 0) {
+//   if (year % 100 == 0) {
+//     if (year % 400 == 0) {
+//       console.log("This year is leap year");
+//     } else {
+//       console.log("This year is not a leap year");
+//     }
+//   } else {
+//     console.log("This year is a leap year");
+//   }
+// } else {
+//   console.log("This year is not a leap year");
+// }
+
+// const leap = (year) =>{
+//     return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+// }
+
+// for (let i = 2020; i <= 2050; i++) {
+// let d = new Date(2024, 2, 19);
+// if (d.getDay() === 4) {
+//   console.log(d);
+// }
+// }
+
+// function cubes(a) {
+// 	let cube = a;
+// 	return cube;
+// }
+// let result = cubes(3);
+// console.log(result);
+
+// THERE ARE FOUR WAYS TO CREATE A NEW DATE OBJECT
+
+// let date = new Date().toLocaleString();
+// let date1 = new Date().toString();
+
+// let d = new Date(2023, 0, 25, 19, 33, 30, 40);
+// console.log(d.toLocaleString());
+
+// let d = new Date("may 19, 2023 17:50:00");
+// console.log(d.toLocaleString());
+// console.log(d.toLocaleTimeString());
+// console.log(d.toLocaleDateString());
+// console.log(d.toLocaleString());
+
+// let date = new Date();
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.setFullYear(2023, 5, 2));
+// console.log(date.getTime());
+// console.log(date.setHours(5));
+
+// let date = new Date();
+// let d = date.getDate();
+// let d1 = date.getMonth() + 1;
+// let d2 = date.getFullYear();
+// console.log(d);
+// console.log(d1);
+// console.log(d2);
+// console.log(d + "-" + d1 + "-" + d2 + "," + "" + date.toLocaleDateString());
+
+// for (let year = 2014; year <= 2050; year++) {
+//   let date = new Date(year, 0, 1);
+//   if (date.getDay() == 0) {
+//     console.log(date.toLocaleDateString());
+//   }
+// }
+
+// let date = new Date();
+// console.log(date.toLocaleDateString());
+
+// function getNoDays(month, year) {
+//   let date = new Date(year, month, 0).getDate();
+//   return date;
+// }
+// console.log(getNoDays(2, 2023));
+
+// let arr = ['jan', 'feb', 'march', 'may', 'june', 'july', 'august', 'sept', 'october', 'november', 'december']
+// function date(date, month, year) {
+//   let d = new Date(year, month, date).arr[];
+//   return d;
+// }
+// console.log(date(19, 4, 2003));
+
+// console.log(Math.PI);
+
+// let num = 10.1;
+// console.log(Math.round(num));
+// console.log(Math.pow(2, 3));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.sqrt(2));
+// console.log(Math.abs(-8));
+// console.log(Math.min(1, 2, 3, 4));
+// console.log(Math.max(1, 2, 3, 4));
+// console.log(Math.random() * 10);
+// console.log(Math.trunc(33.949));
+
+// alert(location.href);
+// if (confirm("Want to visit my website?")) {
+//   location.href = "https://www.google.com";
+// }
+
+// let d = new Date();
+// console.log(d.setFullYear(2020));
+
+// ARRAY DESTRUCTURING
+
+// let array = ["Apple", "orange", "banana", "grapes", "mango"];
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+
+// let [a, b, c, d, e, f = "Strawbery"] = array;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+
+// OBJECT DESTRUCTURING
+
+// let obj = {
+//   name: "hanish",
+//   lastName: "tharwani",
+//   age: 19,
+// };
+// let a = obj.name;
+// let b = obj.lastName;
+// let c = obj.age;
+// console.log(a, b, c);
+
+// let { name, lastName, age, school = "aps" } = obj;
+// console.log(name);
+
+// OBJECT PROPERTIES
+// we can now use dynamic properties
+
+// let names = "hanish";
+// let obj = {
+//   [names]: "how are you",
+//   lastName: "tharwani",
+//   [10 + 5]: 19,
+// };
+// console.log(obj);
+
+// no need to write key and value, if both are same
+
+// let names = "hanish";
+// let age = "19";
+// let obj = { names, age };
+// console.log(obj);
+
+// SPREAD OPERATOR
+
+// let color = ["red", "green", "purple", "pink", "kala", "safed"];
+// let color1 = [...color, "black", "jamuni"];
+// console.log(color1);
+
+// ECMASCRIPT 7 FEATURES
+
+// 1. array include
+// let color = ["red", "green", "purple", "pink", "kala", "safed"];
+// let isPresent = color.includes("red");
+// console.log(isPresent);
+
+// 2. exponential operator
+// console.log(2 ** 2);
+// console.log(2 * 9);
+
+// ECMASCRIPT 8 FEATURES
+// let message = 'My name is hanish';
+// console.log(message);
+// console.log(message.padStart(5));
+// console.log(message.padEnd(5));
+
+// let obj = {
+//   names: "how are you",
+//   lastName: "tharwani",
+//   age: 19,
+// };
+// console.log(Object.values(obj));
+// let arrObj = console.log(Object.entries(obj));
+// console.log(Object.fromEntries(arrObj));
+
+// ECMASCRIPT 9 FEATURES
+
+// let obj = {
+//   names: "how are you",
+//   lastName: "tharwani",
+//   age: 19,
+// };
+// let obj1 = {
+//   ...obj,
+//   school: "aps",
+// };
+
+// console.log(obj1);
+
+// ES 2019
+// HOW TO FLATTEN AN ARRAY
+// converting 2d and 3d array into one dimensional array
+
+// let arr = [
+//   ["zone1", "zone2"],
+//   ["zone3", "zone4"],
+//   ["zone5", "zone6"],
+//   ["zone7", ["zone8", "zone9"]],
+// ];
+// let flatArr = arr.reduce((accum, currVal) =>{
+//     return accum.concat(currVal)
+// })
+// console.log(arr.flat(2)); //infinty
+
+// ES 2014
+
+// "use strict";
+// let x = 3.14;
+// console.log(x);
+
+// CALCULATOR
+
+// CALL BACK FUNCTION
+// const subs = (a, b) => {
+//   return a - b;
+// };
+// const add = (a, b) => {
+//   return a + b;
+// };
+// const mult = (a, b) => {
+//   return a * b;
+// };
+
+// HIGHER ORDER FUNCTION
+// const calculator = (num1, num2, operator) => {
+//   return operator(num1, num2);
+// };
+// console.log(calculator(5, 3, subs));
+
+// ASYNCHRONUS JAVASCRIPT
+
+// 1. HOISTING IN JAVASCRIPT
+// console.log(names);
+// var names;
+// names = "hanish";
+
+// HOW IT WILL BE IN OUTPUT DURING CREATION PHASE
+
+// LEXICAL SCOPING
+// let a = "hello"; // GLOBAL SCOPE
+// const first = () => {
+//   let b = "How are you";
+
+//   const second = () => {
+//     let c = "Hi kese ho";
+//     console.log(a + b + c);
+//   };
+
+//   second();
+// };
+// first();
+
+// CLOSURES
+
+// const outerFun = (a) => {
+//   let b = 10;
+//   const innerFun = () => {
+//     let sum = a + b;
+//     console.log(`the sum of the two no is ${sum}`);
+//   };
+//   innerFun();
+// };
+// outerFun(5);
+
+// DIFFERENCE BETWEEN SYNCHRONOUS AND ASYNCHRONOUS JAVASCRIPT
+
+// SYNCRONOUS JAVASCRIPT
+// const fun2 = () => {
+//   console.log(`Function 2 is called`);
+// };
+
+// const fun1 = () => {
+//   console.log(`Function 1 is called`);
+//   fun2();
+//   console.log(`Function 1 is called again`);
+// };
+
+// fun1();
+
+// ASYNCHRONOUS JAVASCRIPT
+// const fun2 = () => {
+//   setTimeout(() => {
+//     console.log(`Function 2 is called`);
+//   }, 2000);
+// };
+
+// const fun1 = () => {
+//   console.log(`Function 1 is called`);
+//   fun2();
+//   console.log(`Function 1 is called Again`);
+// };
+
+// fun1();
+
+// EVENT LOOP IN JAVASCRIPT
+
+// FUNCTION CURRYING
+// function sum(num1) {
+//   return function (num2) {
+//     return function (num3) {
+//       console.log(num1 + num2 + num3);
+//     };
+//   };
+// }
+// sum(5)(3)(8);
+
+// CALL BACK HELL
+
+// setTimeout(() => {
+//   console.log(`1 works is done`);
+//   setTimeout(() => {
+//     console.log(`2 works is done`);
+//     setTimeout(() => {
+//       console.log(`3 works is done`);
+//       setTimeout(() => {
+//         console.log(`4 works is done`);
+//         setTimeout(() => {
+//           console.log(`5 works is done`);
+//           setTimeout(() => {
+//             console.log(`6 works is done`);
+//           }, 1000);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+// JSON
+
+// let obj = {
+//   key1: "text",
+//   key2: true,
+//   key3: 4,
+// };
+// let obj_string = JSON.stringify(obj);  // converting object to json
+// console.log(obj_string);
+// let obj1 = JSON.parse(obj_string);  // converting json to object
+// console.log(obj1);
